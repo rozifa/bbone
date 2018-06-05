5b14f71f7e209c60090824a22f381fdcf0a48dbc
Din Becevic <dinbecevic@gmail.com>
2018-06-05 17:00:22 -0400

    added timing functions to check efficancy

--- test_functions.h
+++ test_functions.h
@@ -2,17 +2,35 @@
 
 //These functions are to be used in test_linear.c to execute the enccassary tests
 
-//print entries of a neon array 
+//timeing??
+
+//maybe its better to start calling these thigns through Cython in a 
+//Jupyter notebook that way we can easily ocmapre graph times?
+//Pobablly too much work though when we can jsut output to terminal quickly.. :(
+void efficency(){
+
+	//do stuff...
+
+}
+
+//print matrix 
 void print_matrix(Matrix matrix){
 	printf("%f %f %f\n", matrix.a.x, matrix.b.x, matrix.c.x);
 	printf("%f %f %f\n", matrix.a.y, matrix.b.y, matrix.c.y);
 	printf("%f %f %f\n", matrix.a.z, matrix.b.z, matrix.c.z);
 }
 
+//prints vector 
 void print_vector(fVector vector){
 	printf("%f\n", vector.x);
 	printf("%f\n", vector.y);
 	printf("%f\n", vector.z);
 }
+
+//print NEON vector
+
+//print NEON matrix
+
+
 
 
