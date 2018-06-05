c8d4e6c16544fd66976f3b5aa90797a86b72e86e
Din Becevic <dinbecevic@gmail.com>
2018-06-05 16:04:17 -0400

    replaced hard coded with print functions

--- test_linear.c
+++ test_linear.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <arm_neon.h>
 #include "linear_ops.h"
-#include "neon_wrappers.h" 
+#include "neon_wrappers.h"
+#include "test_function.h"
 
 //test function
 // Here we want to call our various data structures 
@@ -51,34 +52,46 @@ int main(){
 	printf("%f %f\n", com1.one.a.y, com1.one.b.y);
 
 	Matrix m3 = MatAdd(m1, m2);
-	printf("%f %f %f\n", m3.a.x, m3.b.x, m3.c.x);
-	printf("%f %f %f\n", m3.a.y, m3.b.y, m3.c.y);
-	printf("%f %f %f\n", m3.a.z, m3.b.z, m3.c.z);
+	print_matrix(m3);
+
+	//printf("%f %f %f\n", m3.a.x, m3.b.x, m3.c.x);
+	//printf("%f %f %f\n", m3.a.y, m3.b.y, m3.c.y);
+	//printf("%f %f %f\n", m3.a.z, m3.b.z, m3.c.z);
 
 	Matrix m4 = MatSub(m3, m1);
-	printf("%f %f %f\n", m4.a.x, m4.b.x, m4.c.x);
-	printf("%f %f %f\n", m4.a.y, m4.b.y, m4.c.y);
-	printf("%f %f %f\n", m4.a.z, m4.b.z, m4.c.z);
+	print_matrix(m4);
+	
+	//printf("%f %f %f\n", m4.a.x, m4.b.x, m4.c.x);
+	//printf("%f %f %f\n", m4.a.y, m4.b.y, m4.c.y);
+	//printf("%f %f %f\n", m4.a.z, m4.b.z, m4.c.z);
 
 	fVector mv5 = transform(v1, m4);
-	printf("%f\n", mv5.x);
-	printf("%f\n", mv5.y);
-	printf("%f\n", mv5.z);
+	print_vector(mv5);
+
+	//printf("%f\n", mv5.x);
+	//printf("%f\n", mv5.y);
+	//printf("%f\n", mv5.z);
 
 	Matrix tm2 = transpose(m2);
-	printf("%f %f %f\n", tm2.a.x, tm2.b.x, tm2.c.x);
-	printf("%f %f %f\n", tm2.a.y, tm2.b.y, tm2.c.y);
-	printf("%f %f %f\n", tm2.a.z, tm2.b.z, tm2.c.z);
+	print_matrix(tm2);
+
+	//printf("%f %f %f\n", tm2.a.x, tm2.b.x, tm2.c.x);
+	//printf("%f %f %f\n", tm2.a.y, tm2.b.y, tm2.c.y);
+	//printf("%f %f %f\n", tm2.a.z, tm2.b.z, tm2.c.z);
 
 	Matrix tm2bym2 = mat_mult(m2, tm2);
-	printf("%f %f %f\n", tm2bym2.a.x, tm2bym2.b.x, tm2bym2.c.x);
-	printf("%f %f %f\n", tm2bym2.a.y, tm2bym2.b.y, tm2bym2.c.y);
-	printf("%f %f %f\n", tm2bym2.a.z, tm2bym2.b.z, tm2bym2.c.z);
+	print_matrix(tm2bym2);
+
+	//printf("%f %f %f\n", tm2bym2.a.x, tm2bym2.b.x, tm2bym2.c.x);
+	//printf("%f %f %f\n", tm2bym2.a.y, tm2bym2.b.y, tm2bym2.c.y);
+	//printf("%f %f %f\n", tm2bym2.a.z, tm2bym2.b.z, tm2bym2.c.z);
 
 	Matrix tm2bym2_2 = MatrixByMatrixTranspose(m2, m2);
-	printf("%f %f %f\n", tm2bym2_2.a.x, tm2bym2_2.b.x, tm2bym2_2.c.x);
-	printf("%f %f %f\n", tm2bym2_2.a.y, tm2bym2_2.b.y, tm2bym2_2.c.y);
-	printf("%f %f %f\n", tm2bym2_2.a.z, tm2bym2_2.b.z, tm2bym2_2.c.z);
+	print_matrix(tm2bym2_2);
+
+	//printf("%f %f %f\n", tm2bym2_2.a.x, tm2bym2_2.b.x, tm2bym2_2.c.x);
+	//printf("%f %f %f\n", tm2bym2_2.a.y, tm2bym2_2.b.y, tm2bym2_2.c.y);
+	//printf("%f %f %f\n", tm2bym2_2.a.z, tm2bym2_2.b.z, tm2bym2_2.c.z);
 
 return 0;
 }
