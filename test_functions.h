6e238be499126ca75635ef5041d8f1f029536e81
Din Becevic <dinbecevic@gmail.com>
2018-06-06 17:51:29 -0400

    debug

--- test_functions.h
+++ test_functions.h
@@ -29,7 +29,7 @@ void print_vector(fVector vector){
 }
 
 void print_assess(double time1, double time2){
-	double improvement = abs((time1 - time2)/time1);
+	double improvement = (time1 - time2)/time1;
 	printf("The efficiency improvement is: %f.\n", improvement);
 }
 //print NEON vector
