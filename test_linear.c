ffdf1a86fb6f1e9496bd8ec8f5ea9294e3312fca
Din Becevic <dinbecevic@gmail.com>
2018-06-13 13:34:59 -0400

    stay strong

--- test_linear.c
+++ test_linear.c
@@ -197,7 +197,7 @@ int main(){
 	fVector v3 = {.x = 93.93, .y = 16.0, .z = 7.0};
 	Matrix mat_boy = {.a = v1, .b = v2, .c = v3};
 	float32x4x4_t neon_mat_boy = Matrix_wrapper(mat_boy);
-	struct_mat_boy = float32_to_lVector(neon_mat_boy);
+	lVector struct_mat_boy = float32x4x4_to_lVector(neon_mat_boy);
 
 	lVector back_boy4 = float32_to_lVector(neon_mat_boy);
 	print_lVector(back_boy4);
